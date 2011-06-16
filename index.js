654477ebdaef09e4b9a79f0831ff297806f980e7
Heather Arthur <fayearthur@gmail.com>
2011-06-16 10:26:40 -0700

    remove commented out code

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ for (var func in conversions) {
   var pair = /(\w+)2(\w+)/.exec(func),
       from = pair[1],
       to = pair[2];
-  exports[from] = exports[from] || {};
 
   // export rgb2hsl and ["rgb"]["hsl"]
-  exports[func] = exports[from][to] = (function(func) { 
+  exports[from] = exports[from] || {};
+
+  exports[from][to] = exports[func] = (function(func) { 
     return function(arg) {
       if (typeof arg == "number")
         arg = Array.prototype.slice.call(arguments);
@@ -34,67 +35,4 @@ for (var func in conversions) {
       return val;
     }
   })(func);
-
 }
-
-/*
-exports["rgb"]  = {
-  "hsl": exports.rgb2hsl,
-  "hsv": exports.rgb2hsv,
-  "cmyk": exports.rgb2cmyk,
-  "keyword": exports.rgb2keyword,
-  "xyz": exports.rgb2xyz,
-  "lab": exports.rgb2lab,
-}
-
-exports["hsl"]  = {
-  "rgb": exports.hsl2rgb,
-  "hsv": exports.hsl2hsv,
-  "cmyk": function(args) {
-    return exports.rgb2cmyk(exports.hsl2rgbRaw(args));
-  },
-  "keyword": function(args) {
-    return exports.rgb2keyword(exports.hsl2rgbRaw(args));
-  }
-}
-
-exports["hsv"] = {
-  "rgb": exports.hsv2rgb,
-  "hsl": exports.hsv2hsl,
-  "cmyk": function(args) {
-    return exports.rgb2cmyk(exports.hsv2rgbRaw(args));
-  },
-  "keyword": function(args) {
-    return exports.rgb2keyword(exports.hsv2rgbRaw(args));
-  }
-}
-
-exports.cmyk = {
-  "rgb": exports.cmyk2rgb,
-  "hsl": function(args) {
-    return exports.rgb2hsl(exports.cmyk2rgbRaw(args));
-  },
-  "hsv": function(args) {
-    return exports.rgb2hsv(exports.cmyk2rgbRaw(args));
-  },
-  "keyword": function(args) {
-    return exports.rgb2keyword(exports.cmyk2rgbRaw(args));
-  }
-}
-
-exports.keyword = {
-  "rgb": exports.keyword2rgb,
-  "hsl": function(args) {
-    return exports.rgb2hsl(exports.keyword2rgb(args));
-  },
-  "hsv": function(args) {
-    return exports.rgb2hsv(exports.keyword2rgb(args));
-  },
-  "cmyk": function(args) {
-    return exports.rgb2cmyk(exports.keyword2rgb(args));
-  }
-}
-
-exports.xyz = {
-  "rgb": exports.xyz2rgb
-} */
