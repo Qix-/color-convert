4e10ff7d26ef1702d41cbc6413b61cc9d1a254f5
acterhd <acterhd@gmail.com>
2016-02-03 14:20:57 +0800

    color converts

--- conversions.js
+++ conversions.js
@@ -193,25 +193,28 @@ convert.rgb.hcg = function (rgb) {
 	var chroma = (max - min);
 	var grayscale;
 	var hue;
+
 	if (chroma < 1) {
 		grayscale = min / (1 - chroma);
 	} else {
 		grayscale = 0;
 	}
-	if (chroma > 0) {
-		if (max === r) {
-			hue = ((g - b) / chroma) % 6;
-		} else
-		if (max === g) {
-			hue = 2 + (b - r) / chroma;
-		} else {
-			hue = 4 + (r - g) / chroma + 4;
-		}
-		hue /= 6;
-		hue %= 1;
-	} else {
+
+	if (chroma <= 0) {
 		hue = 0;
+	} else
+	if (max === r) {
+		hue = ((g - b) / chroma) % 6;
+	} else
+	if (max === g) {
+		hue = 2 + (b - r) / chroma;
+	} else {
+		hue = 4 + (r - g) / chroma + 4;
 	}
+
+	hue /= 6;
+	hue %= 1;
+
 	return [hue * 360, chroma * 100, grayscale * 100];
 };
 
@@ -288,28 +291,28 @@ convert.hsl.hsv = function (hsl) {
 convert.hsl.hcg = function (hsl) {
 	var s = hsl[1] / 100;
 	var l = hsl[2] / 100;
-	var c = 0;
+	var c = 1;
+	var f = 0;
+
 	if (l < 0.5) {
 		c = 2.0 * s * l;
 	} else {
 		c = 2.0 * s * (1.0 - l);
 	}
-	var res;
+
 	if (c < 1.0) {
-		var f = (l - 0.5 * c) / (1.0 - c);
-		res = [hsl[0], c * 100, f * 100];
-	} else {
-		res = [hsl[0], c * 100, 0];
+		f = (l - 0.5 * c) / (1.0 - c);
 	}
-	return res;
+
+	return [hsl[0], c * 100, f * 100];
 };
 
 convert.hsv.rgb = function (hsv) {
 	var h = hsv[0] / 60;
 	var s = hsv[1] / 100;
 	var v = hsv[2] / 100;
-	var hi = Math.floor(h) % 6;
 
+	var hi = Math.floor(h) % 6;
 	var f = h - Math.floor(h);
 	var p = 255 * v * (1 - s);
 	var q = 255 * v * (1 - (s * f));
@@ -336,6 +339,7 @@ convert.hsv.hsl = function (hsv) {
 	var h = hsv[0];
 	var s = hsv[1] / 100;
 	var v = hsv[2] / 100;
+
 	var sl;
 	var l;
 
@@ -351,28 +355,32 @@ convert.hsv.hsl = function (hsv) {
 convert.hsv.hcg = function (hsv) {
 	var s = hsv[1] / 100;
 	var v = hsv[2] / 100;
+
 	var c = s * v;
-	var res;
+	var f = 0;
+
 	if (c < 1.0) {
-		var f = (v - c) / (1 - c);
-		res = [hsv[0], c * 100, f * 100];
-	} else {
-		res = [hsv[0], c * 100, 0];
+		f = (v - c) / (1 - c);
 	}
-	return res;
+
+	return [hsv[0], c * 100, f * 100];
 };
 
 convert.hcg.rgb = function (hcg) {
 	var h = hcg[0] / 360;
 	var c = hcg[1] / 100;
 	var g = hcg[2] / 100;
+
 	if (c === 0.0) {
 		return [g * 255, g * 255, g * 255];
 	}
+
+	var pure = [0, 0, 0];
 	var hi = (h % 1) * 6;
 	var v = hi % 1;
-	var pure = [0, 0, 0];
 	var w = 1 - v;
+	var mg = 0;
+
 	switch (Math.floor(hi)) {
 		case 0:
 			pure[0] = 1; pure[1] = v; pure[2] = 0; break;
@@ -387,41 +395,44 @@ convert.hcg.rgb = function (hcg) {
 		default:
 			pure[0] = 1; pure[1] = 0; pure[2] = w;
 	}
-	var mg = (1.0 - c) * g;
-	var rgb = [
+
+	mg = (1.0 - c) * g;
+
+	return [
 		(c * pure[0] + mg) * 255,
 		(c * pure[1] + mg) * 255,
 		(c * pure[2] + mg) * 255
 	];
-	return rgb;
 };
 
 convert.hcg.hsv = function (hcg) {
 	var c = hcg[1] / 100;
 	var g = hcg[2] / 100;
+
 	var v = c + g * (1.0 - c);
-	var res;
+	var f = 0;
+
 	if (v > 0.0) {
-		var f = c / v;
-		res = [hcg[0], f * 100, v * 100];
-	} else {
-		res = [hcg[0], 0, v * 100];
+		f = c / v;
 	}
-	return res;
+
+	return [hcg[0], f * 100, v * 100];
 };
 
 convert.hcg.hsl = function (hcg) {
 	var c = hcg[1] / 100;
 	var g = hcg[2] / 100;
+
 	var l = g * (1.0 - c) + 0.5 * c;
 	var s = 0;
-	if (l < 1.0 && l > 0.0) {
-		if (l < 0.5) {
-			s = c / (2 * l);
-		} else {
-			s = c / (2 * (1 - l));
-		}
+
+	if (l > 0.0 && l < 0.5) {
+		s = c / (2 * l);
+	} else
+	if (l >= 0.5 && l < 1.0) {
+		s = c / (2 * (1 - l));
 	}
+
 	return [hcg[0], s * 100, l * 100];
 };
 
