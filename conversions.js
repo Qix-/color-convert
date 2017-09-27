fd5737640e382fb6ddd50f7ec0f5fc46fb5d86dc
caub <cyril.auburtin@gmail.com>
2017-09-27 11:53:52 +0200

    clearer hsv.rgb

--- conversions.js
+++ conversions.js
@@ -276,30 +276,23 @@ convert.hsl.hsv = function (hsl) {
 };
 
 convert.hsv.rgb = function (hsv) {
-	var h = hsv[0] / 60;
+	var a = hsv[0] / 60;
+	var i = Math.floor(a);
+	var f = a - i;
 	var s = hsv[1] / 100;
 	var v = hsv[2] / 100;
-	var hi = Math.floor(h) % 6;
+	var p = v * (1 - s);
+	var q = v * (1 - f * s);
+	var t = v * (1 - (1 - f) * s);
 
-	var f = h - Math.floor(h);
-	var p = 255 * v * (1 - s);
-	var q = 255 * v * (1 - (s * f));
-	var t = 255 * v * (1 - (s * (1 - f)));
-	v *= 255;
-
-	switch (hi) {
-		case 0:
-			return [v, t, p];
-		case 1:
-			return [q, v, p];
-		case 2:
-			return [p, v, t];
-		case 3:
-			return [p, q, v];
-		case 4:
-			return [t, p, v];
-		case 5:
-			return [v, p, q];
+	switch (i) {
+		case 6:
+		case 0: return [v * 255, t * 255, p * 255];
+		case 1: return [q * 255, v * 255, p * 255];
+		case 2: return [p * 255, v * 255, t * 255];
+		case 3: return [p * 255, q * 255, v * 255];
+		case 4: return [t * 255, p * 255, v * 255];
+		case 5: return [v * 255, p * 255, q * 255];
 	}
 };
 
